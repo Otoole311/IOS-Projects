b2ef8dab5e0eff9c0dd4d5f1b1f3bcb4c38b8e24
Albert Essilfie <essilfie311@gmail.com>
2022-02-25 12:50:05 +0000

    Initial Commit

---
+++ SpinnerApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SpinnerApp
+//
+//  Created by Otoole on 25/02/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
